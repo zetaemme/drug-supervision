0dcd222905d9867c0f857bcf958d84523acca545
JustPrnc <mattia.zorzan@gmail.com>
2018-05-10 17:58:31 +0200

    Updated@10/05_17:58

--- src/Report.java
+++ src/Report.java
@@ -1,2 +1,40 @@
-public class Main {
+public class Report {
+    private final Patient patient;
+    private final Reaction reaction;
+    private final Date report_date;
+    private final Date reaction_date;
+    private final Therapy therapy;
+
+    private Report(Patient patient, Reaction reaction, Date report_date, Date reaction_date, Therapy therapy) {
+        this.patient = patient;
+        this.reaction = reaction;
+        this.report_date = report_date;
+        this.reaction_date = reaction_date;
+        this.therapy = therapy;
+    }
+
+    @Override
+    int hashCode() {
+        return patient.hashCode() ^ reaction.hashCode() ^ report_date.hashCode() ^ reaction_date.hashCode() ^ therapy.hashCode();
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public Reaction getReaction() {
+        return reaction;
+    }
+
+    public Date getReport_date() {
+        return report_date;
+    }
+
+    public Date getReaction_date() {
+        return reaction_date;
+    }
+
+    public Therapy getTherapy() {
+        return therapy;
+    }
 }
