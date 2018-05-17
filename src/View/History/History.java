55c04f52e7b1594c1818a31605946b2fd16c218a
JustPrnc <mattia.zorzan@gmail.com>
2018-05-17 16:44:17 +0200

    Added History in View

--- src/View/History/History.java
+++ src/View/History/History.java
@@ -1,4 +1,48 @@
 package View.History;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class History {
+    private Stage primaryStage;
+    private AnchorPane rootLayout;
+
+    public History(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Drug Supervision - History");
+
+        initRootLayout();
+        showOverview();
+    }
+
+    public void initRootLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(History.class.getResource("rootLayout.fxml"));
+            rootLayout = loader.load();
+
+            primaryStage.setScene(new Scene(rootLayout));
+            primaryStage.show();
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void showOverview() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(History.class.getResource("Overview.fxml"));
+            AnchorPane overview = loader.load();
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
 }
---
+++ src/View/History/rootLayoutController.java
@@ -0,0 +1,4 @@
+package View.History;
+
+public class rootLayoutController {
+}
