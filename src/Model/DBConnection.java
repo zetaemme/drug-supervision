40f247a4f763edaa5e06f2abdc127fc6b52366ec
Exachias <asoglieri@yahoo.it>
2018-05-17 15:52:38 +0200

    Updated17/5_15:52

--- src/Model/DBConnection.java
+++ src/Model/DBConnection.java
@@ -1,18 +1,16 @@
-package Controller;
+package Model;
 
-import java.sql.Connection;
-import java.sql.Statement;
-import java.sql.DriverManager;
+import java.sql.*;
+import java.sql.PreparedStatement;
 
 /**
  * @author Andrea Soglieri e Mattia Zorzan
  */
 
 public class DBConnection {
-    Connection connection = null;
-    Statement statement = null;
+    static Connection connection = null;
 
-    DBConnection() {
+    private static void openConnection () {
         try {
             Class.forName("org.sqlite.JBDC");
             connection = DriverManager.getConnection("jdbc:DB:DrugDB.db");
@@ -20,5 +18,45 @@ public class DBConnection {
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
+    }
+
+    private static void closeConnection() throws SQLException {
+        connection.close();
+    }
+
+    public static void insert(String buffer, String tableName) throws Exception {
+        openConnection();
+
+        PreparedStatement insertQuery;
+
+        switch(tableName){
+            case "Patient":
+                insertQuery = connection.prepareStatement("INSERT INTO " + tableName + "(idPatient, birthday, province, professionn) " +
+                        "VALUES (" + buffer + ")");
+                insertQuery.executeUpdate();
+                break;
+            case "RiskFactor":
+                insertQuery = connection.prepareStatement("INSERT INTO " + tableName + "(idFactor, description, riskLevel)" +
+                        "VALUES (" + buffer + ")");
+                insertQuery.executeUpdate();
+                break;
+            case "Reaction":
+                insertQuery = connection.prepareStatement("INSERT INTO " + tableName + "(Reaction_name, risk, description)" +
+                        "VALUES (" + buffer + ")");
+                insertQuery.executeUpdate();
+                break;
+            case "Report":
+                insertQuery = connection.prepareStatement("INSERT INTO " + tableName + "(idReport, reaction_date, report_date)" +
+                        "VALUES (" + buffer + ")");
+                insertQuery.executeUpdate();
+                break;
+            case "Therapy":
+                insertQuery = connection.prepareStatement("INSERT INTO " + tableName + "(idTherapy, drug, dose, startingDate, endingDate, dailyFrequency" +
+                        "VALUES (" + buffer + ")");
+                insertQuery.executeUpdate();
+                break;
+        }
+
+        closeConnection();
     }
 }
