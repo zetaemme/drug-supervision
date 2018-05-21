cad3612dfa315f5d8d995c70940a4ea9800d2f31
Exachias <asoglieri@yahoo.it>
2018-05-21 15:51:56 +0200

    Added and pushed everything

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+/**
+ * @author Andrea Soglieri e Mattia Zorzan
+ */
 
-public class MainApp {
+import View.Login.Login;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    /**
+     * Metodo main
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     * Metodo start
+     *
+     * @param primaryStage
+     */
+    @Override
+    public void start(Stage primaryStage) {
+        Login login = new Login(primaryStage);
+    }
 }
