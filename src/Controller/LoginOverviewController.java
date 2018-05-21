39a61da268d181825439d0a6a5cb5216babfce45
Exachias <asoglieri@yahoo.it>
2018-05-21 15:50:46 +0200

    Moved and renamed LoginOverviewController.java

--- src/Controller/LoginOverviewController.java
+++ src/Controller/LoginOverviewController.java
@@ -1,9 +1,20 @@
-package View.Login;
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
 
 /**
  * @author Andrea Soglieri e Mattia Zorzan
  */
 
-public class OverviewController {
-    
+public class LoginOverviewController {
+
+    public Button loginButton;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
 }
