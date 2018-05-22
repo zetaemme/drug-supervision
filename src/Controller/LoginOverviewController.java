34d3502f891505b8e198b0b4d64f2c08b6c18be9
JustPrnc <mattia.zorzan@gmail.com>
2018-05-22 13:28:16 +0200

    Added JavaDoc, minor changes to LoginOverviewController

--- src/Controller/LoginOverviewController.java
+++ src/Controller/LoginOverviewController.java
@@ -1,6 +1,7 @@
 package Controller;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -12,9 +13,33 @@ import javafx.scene.control.PasswordField;
 
 public class LoginOverviewController {
 
+    /**
+     * Bottone
+     */
+    @FXML
     public Button loginButton;
+    /**
+     * Campo di testo
+     */
     @FXML
     private TextField username;
+    /**
+     * Campo di testo tipo password
+     */
     @FXML
     private PasswordField password;
+
+    /**
+     * Metodo costruttore nullo
+     */
+    public LoginOverviewController() {
+    }
+
+    /**
+     * Metodo per inizializzare l'interfaccia ddi login
+     */
+    @FXML
+    private void initialize() {
+        username.setOnAction(handleUsername -> handleUsername.);
+    }
 }
