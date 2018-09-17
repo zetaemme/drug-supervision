6590414a78e58f82ffb83d1d220d197159b14142
JustPrnc <mattia.zorzan@gmail.com>
2018-09-17 12:08:09 +0200

    Aggiunta etichetta per il titolo

--- src/View/Login.java
+++ src/View/Login.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Login {
@@ -26,32 +27,44 @@ public class Login {
         final TextField username = new TextField();
         final PasswordField password = new PasswordField();
         final Button loginButton = new Button("Login");
-        final Label label = new Label("");
+        final Label loginLabel = new Label("");
+        final Label titleLabel = new Label("Drug Supervision");
 
         // Set the promptText for the TextField/PasswordField
         username.setPromptText("Username");
         password.setPromptText("Password");
 
-        // Set the sizes for the Fields
+        // Set the sizes for the objects
         username.setMinSize(220, 30);
         username.setMaxSize(230, 30);
 
         password.setMinSize(220, 30);
         password.setMaxSize(230, 30);
 
+        loginButton.setMinSize(Button.USE_PREF_SIZE, Button.USE_PREF_SIZE);
+
+        // Title label options
+        titleLabel.setFont(new Font("Fira Code", 30));
+        titleLabel.setId("titleLabel");
+
         // Set the (x, y) position for the objects
+        titleLabel.setTranslateX(70);
+        titleLabel.setTranslateY(25);
+
         username.setTranslateX(75);
-        username.setTranslateY(140);
+        username.setTranslateY(100);
 
         password.setTranslateX(75);
-        password.setTranslateY(165);
+        password.setTranslateY(125);
 
         loginButton.setTranslateX(330.0);
         loginButton.setTranslateY(275.5);
 
-        loginButton.setMinSize(Button.USE_PREF_SIZE, Button.USE_PREF_SIZE);
+        loginLabel.setTranslateX(165);
+        loginLabel.setTranslateY(160);
 
-        root.getChildren().addAll(username, password, loginButton, label);
+        // Add the objects to the panel
+        root.getChildren().addAll(titleLabel, username, password, loginButton, loginLabel);
 
         primaryStage.setTitle("Drug Supervision - Login");
         primaryStage.setScene(new Scene(root, 400, 400));
@@ -59,9 +72,9 @@ public class Login {
 
         loginButton.setOnAction(e -> {
             if(controller.login(username.getText(), password.getText())) {
-                label.setText("Login Successful!");
+                loginLabel.setText("Login Successful!");
             } else {
-                label.setText("Login Failed!");
+                loginLabel.setText("Login Failed!");
             }
         });
     }
