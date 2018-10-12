da9c7faad4188e9b0f3d85afc395d397eb4026e0
zetaemme <mattia.zorzan@gmail.com>
2018-10-12 20:23:09 +0200

    Completato tutto quello che c'era da completare

--- src/Controller/NewReactionController.java
+++ src/Controller/NewReactionController.java
@@ -1,4 +1,25 @@
 package Controller;
 
+import Model.Utils.DBConnection;
+
+import java.sql.SQLException;
+
 public class NewReactionController {
+    private DBConnection nreConnection;
+
+    public void addNewReaction(int risk, String description) {
+        nreConnection = new DBConnection();
+        nreConnection.openConnection();
+
+        try {
+            nreConnection.statement = nreConnection.connection.createStatement();
+            nreConnection.statement.executeUpdate("INSERT INTO Reaction(risk, desctription) VALUES ('" + risk +"', '"
+                                                    + description + "')");
+        } catch(SQLException sqle) {
+            System.out.println("Error: " + sqle.getMessage());
+            nreConnection.closeConnection();
+        } finally {
+            nreConnection.closeConnection();
+        }
+    }
 }
