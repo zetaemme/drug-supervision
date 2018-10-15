7da4e5df7a5d2038f45ca5a9db3c51e6bad41700
Sogrea <asoglieri@yahoo.it>
2018-10-15 16:47:59 +0200

    Added bugged report

--- src/Controller/NewTherapyController.java
+++ src/Controller/NewTherapyController.java
@@ -2,19 +2,23 @@ package Controller;
 
 import Model.Utils.DBConnection;
 
+import java.sql.Date;
 import java.sql.SQLException;
 
 public class NewTherapyController {
     private DBConnection ntConnection;
 
-    public void addNewTherapy(String drugName, int dose, String startingDate, String endingDate, int dailyFrequency) {
+    public String addNewTherapy(String drugName, int dose, Date startingDate, Date endingDate, int dailyFrequency) {
         ntConnection = new DBConnection();
         ntConnection.openConnection();
 
+        String idTherapy = drugName.substring(0, 3).toUpperCase() + dose + startingDate.toString().replace("-", "")
+                + endingDate.toString().replace("-", "") + dailyFrequency;
+
         try {
             ntConnection.statement = ntConnection.connection.createStatement();
-            ntConnection.statement.executeUpdate("INSERT INTO Therapy(drugName_drug, dose, startingDate, endingDate, dailyFrequency) " +
-                                                    "VALUES ('" + drugName + "', '" + dose + "', '" + startingDate + "', '"
+            ntConnection.statement.executeUpdate("INSERT INTO Therapy(idTherapy,drugName_drug, dose, startingDate, endingDate, dailyFrequency) " +
+                                                    "VALUES ('" + idTherapy + "','" + drugName + "', '" + dose + "', '" + startingDate + "', '"
                                                     + endingDate + "', '" + dailyFrequency + "')");
         } catch(SQLException sqle) {
             System.out.println("Error: " + sqle.getMessage());
@@ -22,5 +26,7 @@ public class NewTherapyController {
         } finally {
             ntConnection.closeConnection();
         }
+
+        return idTherapy;
     }
 }
