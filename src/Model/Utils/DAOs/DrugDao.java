1dbd9c632b58dae280afd3fbae69ee9f4fc46687
zetaemme <mattia.zorzan@gmail.com>
2018-11-14 09:50:18 +0100

    Finito DrugDaoImpl

---
+++ src/Model/Utils/DAOs/DrugDao.java
@@ -0,0 +1,11 @@
+package Model.Utils.DAOs;
+
+import Model.Drug;
+import Model.Exceptions.NullStringException;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface DrugDao {
+    List<Drug> getAllDrugs() throws SQLException, NullStringException;
+}
